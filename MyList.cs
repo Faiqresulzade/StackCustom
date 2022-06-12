cb89a3129860ef4c903fcc5fce5af6c22e03cdc1
Faiqresulzade <104448615+Faiqresulzade@users.noreply.github.com>
2022-06-12 22:27:14 +0400

    added

---
+++ MyList.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CustomList
+{
+    class MyList<T>
+    {
+        
+        private int _count = 0;
+        public int Capacity { get; set; }
+
+        public int Size
+        {
+            get { return _count; }
+        }
+        private T[] data;
+        public bool IsEmpty
+        {
+            get
+            {
+                return Size == 0;
+            }
+        }
+        public MyList(int capacity = 8)
+        {
+
+            if (capacity < 1)
+            { capacity = 1; }
+            this.Capacity = capacity;
+            data = new T[capacity];
+
+        }
+
+        private void Resize()
+        {
+            T[] resized = new T[Capacity * 2];
+            for (int i = 0; i < Capacity; i++)
+            {
+                resized[i] = data[i];
+            }
+            data = resized;
+            Capacity = Capacity * 2;
+        }
+        public void Add(T element)
+        {
+            if (_count == Capacity)
+            {
+                Resize();
+            }
+            data[_count] = element;
+            _count++;
+        }
+
+        public void Clear()
+        {
+            data = new T[Capacity];
+            _count = 0;
+
+        }
+
+        public bool Contains(T value)
+        {
+            for (int i = 0; i < _count; i++)
+            {
+
+                if (data[i].Equals(value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Insert(T element, int index)
+        {
+            if (_count == Capacity)
+            {
+                Resize();
+            }
+
+            for (int i = _count; i > index; i--)
+            {
+                data[i] = data[i - 1];
+
+            }
+            data[index] = element;
+            _count++;
+        }
+        public void Remove(int index)
+        {
+            for (int i = index; i < _count - 1; i++)
+            {
+                data[i] = data[i + 1];
+
+            }
+            data[_count - 1] = default(T);
+            _count--;
+        }
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                yield return data[i];
+            }
+        }
+    }
+}
+    
+
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(2);
+            MyList<int> myList = new MyList<int>();
+            myList.Add(5);
+            myList.Add(6);
+            myList.Add(7);
+            myList.Add(8);
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
