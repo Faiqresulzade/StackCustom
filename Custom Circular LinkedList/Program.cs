14203494b10e2a303edb4d005609ef3b7fd2f6ab
Faiqresulzade <104448615+Faiqresulzade@users.noreply.github.com>
2022-06-12 22:47:03 +0400

    added

---
+++ Custom Circular LinkedList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomCircularLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> linkedList = new LinkedList<int>();
+            MyCircularLinkedList<int> age = new MyCircularLinkedList<int>();
+            age.AddFirst(18);
+            age.AddFirst(20);
+            age.AddFirst(22);
+            age.AddFirst(13);
+            age.AddLast(9);
+            age.AddFirst(18);
+            foreach (var item in age)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
