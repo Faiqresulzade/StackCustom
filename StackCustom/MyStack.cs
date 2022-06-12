7a96ce1103b0794816e0e1c2862b17bf5aa29b11
Faiqresulzade <faigbr@code.edu.az>
2022-06-12 15:49:53 +0400

    Proje dosyası ekle.

---
+++ StackCustom/MyStack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackCustom
+{
+    class MyStack <T> :IEnumerable
+    {
+        private int Count { get; set; }
+        private T []_arr ;
+        public int Capacity { get; set; }
+        public MyStack()
+        {
+            Capacity = 4;
+            _arr = new T[Capacity];
+        }
+        public MyStack(int capacity)
+        {
+            Capacity = capacity;
+            _arr = new T[capacity];
+        }
+        public void Push(T value )
+        {
+            if (Count == Capacity)
+            {
+                Capacity *= 2;
+                Array.Resize(ref _arr, Capacity);
+            }
+            _arr[Count] = value;
+            Count++;
+        }
+        public void Pop()
+        {
+            if (Count == 0) Console.WriteLine("element elave etmemisiniz!!");
+            for (int i = Count; i >0 ; i--)
+            {
+                _arr[Count] = _arr[Count - 1];
+            }
+            Count--;
+        }
+        public T Peek()
+        {
+            if (Count == 0) Console.WriteLine("element elave etmemisiniz!!");
+            return _arr[Count-1];
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = Count-1; i >=0; i--)
+            {
+                yield return _arr[i];
+            }
+        }
+    }
+}
---
+++ StackCustom/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackCustom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            MyStack<int> num = new MyStack<int>();
+            num.Push(2);
+            num.Push(3);
+            num.Push(4);
+            num.Pop();
+            Console.WriteLine(num.Peek());
+            foreach (var item in num)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
